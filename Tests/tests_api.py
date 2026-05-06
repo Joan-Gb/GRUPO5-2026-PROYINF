555e9bfe8ee686414e276236046d25b7c9749ebd
matrixan <matias.huiscan@gmail.com>
2026-05-05 22:11:34 -0400

    Test hecho y bdd con controller de simulacion actualizados para HU-009

---
+++ Tests/tests_api.py
@@ -0,0 +1,41 @@
+import unittest
+import requests
+
+class TestPrestamosAPI(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        print("Iniciando tests de API...")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("Tests de API finalizados.")
+
+    
+    def test_cp01_simulacion_valida(self):
+        datos = {"cliente_id": "1", "monto": 5000000, "plazo_meses": 48, "tasa_anual": 0.15}
+        respuesta = requests.post("http://localhost:3000/api/simulations", json=datos)
+        
+        self.assertEqual(respuesta.status_code, 201)
+
+    def test_cp02_simulacion_invalida(self):
+        datos = {"cliente_id": "1", "plazo_meses": 48, "tasa_anual": 0.15} # Falta monto
+        respuesta = requests.post("http://localhost:3000/api/simulations", json=datos)
+        
+        self.assertEqual(respuesta.status_code, 400)
+
+    def test_cp03_sugerida_frontera(self):
+        datos = {"cliente_id": "1", "renta_liquida": 1000000, "antiguedad_laboral": 11, "plazo_meses": 36}
+        respuesta = requests.post("http://localhost:3000/api/simulations/sugerida", json=datos)
+        
+        self.assertEqual(respuesta.status_code, 201)
+        self.assertEqual(respuesta.json()["evaluacion_riesgo"]["probabilidad"], "Baja")
+
+    def test_cp04_sugerida_renta_cero(self):
+        datos = {"cliente_id": "1", "renta_liquida": 0, "antiguedad_laboral": 24, "plazo_meses": 36}
+        respuesta = requests.post("http://localhost:3000/api/simulations/sugerida", json=datos)
+        
+        self.assertEqual(respuesta.status_code, 400)
+
+if __name__ == '__main__':
+    unittest.main()
